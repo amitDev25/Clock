e3ba8cb4846d4c734be5dc701cb649d295aa1d8d
amitDev25 <110351067+amitDev25@users.noreply.github.com>
2024-03-14 19:47:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+setInterval(() => {
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
+
+
+
+
+    let day = date.toLocaleDateString();
+
+    let hrotate = (30 * hour) + (minute / 2)
+    let mrotate = (6 * minute) + (second / 10)
+    let srotate = 6 * second
+
+    document.getElementById("hour").style.transform = `rotate(${hrotate}deg)`
+    document.getElementById("minute").style.transform = `rotate(${mrotate}deg)`
+    document.getElementById("second").style.transform = `rotate(${srotate}deg)`
+
+    if (second < 10) {
+        second = `0${second}`
+    }
+
+    if (hour > 12) {
+        hour = hour - 12
+        if (hour < 10) {
+            hour = `0${hour}`
+        }
+        newHTML = `<p>The Time is : ${hour} : ${minute} : ${second} PM </p> <p>Today's Date is : ${day} </p>`
+
+    }
+    else {
+        newHTML = `<p>The Time is : ${hour} : ${minute} : ${second} AM </p> <p>Today's Date is : ${day} </p>`
+
+    }
+
+    document.getElementById("digitalClock").innerHTML = newHTML
+
+    // console.log(`${hour} : ${minute} : ${second}`)
+
+}, 1000);
